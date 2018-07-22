3d408533fb3093e9bf2821f755079d894ad79af8
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-22 21:19:20 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,14 @@
 class CreateShows < ActiveRecord::Migration[5.1]
 
-end 
+def change
+  create_table shows do |t|
+    t.string :name,
+    t.string :network,
+    t.string :day,
+    t.integer :rating
+  end
+end
+
+
+
+end
